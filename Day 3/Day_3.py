b8e9d7fad78a6b4467e38d4373b3d455ae028297
de-marauder <65220956+de-marauder@users.noreply.github.com>
2022-04-08 00:05:57 +0100

    Create Day_3.py

---
+++ Day 3/Day_3.py
@@ -0,0 +1,17 @@
+def palindrome_finder(num):
+  result = []
+  counter = 0
+  while counter < num:
+    #convert number to string and split in into an array
+    numArr = list(str(counter))
+    #compare num array with a reversed copy and add the counter to the result array if they are equal
+    if numArr == numArr[::-1]:
+      result.append(counter)
+    counter +=1 
+    
+  return result, len(result), num
+
+
+result, amount, max_num = palindrome_finder(500)
+
+print(f"The palindromic numbers below {max_num} are {amount} in number. \nThey include:\n {result}")
